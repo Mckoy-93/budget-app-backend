162612aa5f132fbdd4921c16ec8ca097316e9d24
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-04-19 01:39:16 -0400

    README.md intiitialized

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // create and config my express app;
 
 const express = require("express");
+const transactionsController = require("./controllers/transactionsController.js")
 const cors = require("cors")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/transactions", transactionsController)
+
 app.get("/", (req, res) => {
-    res.send("Welcome to budget app");
+    res.send("Welcome to the Budget App");
 })
-//const transactionsController = require("./controllers/transactionsController.js")
-//const cors = require("cors")
+
+app.get("*")
 
 module.exports = app;
